de5450467449216288a8c074fbc2f6bf2d5b03a6
dsiri12 <dsiri12@gmail.com>
2024-10-21 23:12:37 -0400

    one more necessary change I've made to the same file

--- src/services/apiClient.ts
+++ src/services/apiClient.ts
@@ -1,8 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-    // baseURL: "https://api.rawg.io/api",
-    // params: {
-    //     // key: '202517cc716e47ca9e325ee54c952abd'
-    // }
+    
 })
